0572587aec08b474e242572b66adb5e266f9a48a
weiwei1128 <justbearcry@gmail.com>
2017-05-03 15:26:51 +0200

    Member created

---
+++ SAD_Project2017/SAD_Project2017/Models/Member.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SAD_Project2017.Models
+{
+    public class Member
+    {
+        public int id { get; set; }
+        public String firstname { get; set; }
+        public String lastname { get; set; }
+        public String username { get; set; }
+        public String address { get; set; }
+    }
+}
