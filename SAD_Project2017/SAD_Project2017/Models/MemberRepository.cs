d2b95f2d50a6aaef2ef1a81121449bed85b3b9cd
weiwei1128 <justbearcry@gmail.com>
2017-05-03 15:28:43 +0200

    MemberRepository created

---
+++ SAD_Project2017/SAD_Project2017/Models/MemberRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SAD_Project2017.Models
+{
+    //**repository pattern**
+    // Define a generic interface for the repository
+    public interface MemberRepository
+    {
+        System.Collections.Generic.IEnumerable<Member> GetAll();
+        Member Get(int id);
+        Member Add(Member item);
+        void Remove(int id);
+        Boolean Update(Member item);
+    }
+}
