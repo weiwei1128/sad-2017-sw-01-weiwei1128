dac211216534cad860895bcf44b3cb6d722c092b
weiwei1128 <justbearcry@gmail.com>
2017-05-04 16:23:55 +0200

    add another field in Member

--- SAD_Project2017/SAD_Project2017/Models/Member.cs
+++ SAD_Project2017/SAD_Project2017/Models/Member.cs
@@ -4,6 +4,8 @@ namespace SAD_Project2017.Models
     public class Member
     {
         public int id { get; set; }
+        //error or success
+        public String message { get; set; }
         public String firstname { get; set; }
         public String lastname { get; set; }
         public String username { get; set; }
--- SAD_Project2017/SAD_Project2017/Models/Member_R.cs
+++ SAD_Project2017/SAD_Project2017/Models/Member_R.cs
@@ -5,17 +5,17 @@ namespace SAD_Project2017.Models
     public class Member_R : MemberRepository
     {
         private List<Member> Members = new List<Member>();
-		private int next_id = 1;
+        private int next_id = 1;
 
-		public Member_R()
+        public Member_R()
         {
-			//default data
-            Add(new Member { firstname = "John", lastname = "Wu", username = "sunshine00",address = "downtown" });
-			Add(new Member { firstname = "David", lastname = "Becker", username = "thunder12", address = "city center,ghost Rd." });
-			Add(new Member { firstname = "Mary", lastname = "Xiang", username = "wholepackage", address = "uy street no. 5" });
-            Add(new Member { firstname = "Mary", lastname = "Delod", username = "superhero94", address = "polly district" });
-            Add(new Member { firstname = "Cherry", lastname = "Parker", username = "cherryP", address = "hahn road, room 18" });
-            
+            //default data
+            Add(new Member { firstname = "John", message = "", lastname = "Wu", username = "sunshine00", address = "downtown" });
+            Add(new Member { firstname = "David", message = "", lastname = "Becker", username = "thunder12", address = "city center,ghost Rd." });
+            Add(new Member { firstname = "Mary", message = "", lastname = "Xiang", username = "wholepackage", address = "uy street no. 5" });
+            Add(new Member { firstname = "Mary", message = "", lastname = "Delod", username = "superhero94", address = "polly district" });
+            Add(new Member { firstname = "Cherry", message="",lastname = "Parker", username = "cherryP", address = "hahn road, room 18" });
+
         }
 
         public Member Add(Member item)
@@ -30,7 +30,7 @@ namespace SAD_Project2017.Models
         public Member Get(int id)
         {
             Member item = Members.Find(p => p.id == id);
-            if(item==null)
+            if (item == null)
                 throw new ArgumentNullException();
             return item;
         }
@@ -42,20 +42,20 @@ namespace SAD_Project2017.Models
 
         public void Remove(int id)
         {
-            Members.RemoveAll(p=>p.id==id);
+            Members.RemoveAll(p => p.id == id);
         }
 
         public bool Update(Member item)
         {
             //Remove and add
-			if (item == null)
-				throw new ArgumentNullException();
+            if (item == null)
+                throw new ArgumentNullException();
             int index = Members.FindIndex(p => p.id == item.id);
-			if (index == -1)
-				return false;
+            if (index == -1)
+                return false;
             Members.RemoveAt(index);
-			Members.Add(item);
-			return true;
+            Members.Add(item);
+            return true;
         }
     }
 }
