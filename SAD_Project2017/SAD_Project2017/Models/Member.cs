f82f955208f0144769d5b852b2cdbe495f07019b
weiwei1128 <justbearcry@gmail.com>
2017-05-07 15:56:31 +0200

    removed the inherit field

--- SAD_Project2017/SAD_Project2017/Models/Member.cs
+++ SAD_Project2017/SAD_Project2017/Models/Member.cs
@@ -3,11 +3,9 @@ namespace SAD_Project2017.Models
 {
     public class Member :Message
     {
-        public int id { get; set; }
         //error or success
         public Boolean returnCode { get; set; }
-        //message
-        public String message { get; set; }
+        //message}
         public String firstname { get; set; }
         public String lastname { get; set; }
         public String username { get; set; }
